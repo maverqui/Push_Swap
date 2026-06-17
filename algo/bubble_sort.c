3024eb4aeb653da445a41f75a2b03b49fe9d5303
mqe verauin <mae.verquin@learner.42.tech>
2026-06-17 20:41:55 +0200

    bubble

--- algo/bubble_sort.c
+++ algo/bubble_sort.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   bubble_sort.c                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: maeverqu <maeverqu@student.42.fr>          +#+  +:+       +#+        */
+/*   By: maeverqu <mae.verquin@learner.42.tech>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/05/29 13:31:50 by maeverqu          #+#    #+#             */
-/*   Updated: 2026/06/12 15:45:47 by maeverqu         ###   ########.fr       */
+/*   Updated: 2026/06/17 20:41:34 by maeverqu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,23 @@
 
 void	ft_bubble_sort(t_list **stack_a)
 {
+	int		size;
+	int		i; // relance la boucle j
+	int		j; // range + grand nb
 	
+	size = ft_lstsize(*stack_a);
+	i = 0;
+	while (i < size - 1)
+	{
+		j = 0;
+		while (j < size - 1)
+		{	
+			if ((*stack_a)->value > (*stack_a)->next->value)
+				sa(stack_a);
+			ra(stack_a);
+			j++;
+		}
+		ra(stack_a);
+		i++;
+	}
 }
